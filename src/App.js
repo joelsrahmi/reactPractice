40d53a029d4c98c164af5bdaa2473a3d7f37065b
julisah <yulizaa.rahmi@gmail.com>
2017-07-09 22:57:13 +0700

    componentRender

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-//--this is JSX 
-// class App extends React.Component {
-//     render(){
-//         return <h1>Hello Joels! </h1>
-//     }
-// }
-// 
-
-// class App extends React.Component {
-//     render(){
-//         return React.createElement('h1',// null, 'Hello Joels!')
-//     }
-// }
-
-const App = () => <h1>Hello stateless</h1>
+ class App extends React.Component {
+     render(){
+         return(
+            <div>
+                <h1>Hello Joels!</h1>
+                <b>Bold</b>
+            </div>
+         )
+     }
+ }
 
 export default App
