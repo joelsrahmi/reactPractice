2cb9c9c2fba5209428bded28c9b40f654b79b63b
julisah <yulizaa.rahmi@gmail.com>
2017-07-09 23:55:11 +0700

    setStats

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
  class App extends React.Component {
-     render(){
-         let txt = this.props.txt
-         return <h1>{this.props.txt}</h1>
-
-         //return <h1>{this.props.txt}</h1>
+     constructor(){
+         super();
+         this.state = {
+             txt: 'this is the state txt',
+             cat: 5
+         }
      }
- }
+     update(e){
+         this.setState({txt: e.target.value})
+     }
+     render(){
+        return (
+            <div>
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h1>{this.state.txt} - {this.state.cat}</h1>
+            </div>
+        ) 
 
- App.propTypes ={
-     txt: React.PropTypes.string,
-     cat: React.PropTypes.number.isRequired
- }
-
- App.defaultProps = {
-     txt: "this is the default text"
+     }
  }
 
 export default App
