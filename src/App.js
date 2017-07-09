159c1031355dd981c796c4635768d690304035c5
julisah <yulizaa.rahmi@gmail.com>
2017-07-09 21:32:10 +0700

    helloworld

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const App = () => <h1>Hello World</h1>
+//--this is JSX 
+// class App extends React.Component {
+//     render(){
+//         return <h1>Hello Joels! </h1>
+//     }
+// }
+// 
+
+// class App extends React.Component {
+//     render(){
+//         return React.createElement('h1',// null, 'Hello Joels!')
+//     }
+// }
+
+const App = () => <h1>Hello stateless</h1>
 
 export default App
